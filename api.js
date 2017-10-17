0d6dd8059442f09bf915beec375a4df8fc071e8f
Jonas Windey <jonas.windey@gmail.com>
2017-10-17 18:13:00 +0200

    Add API

---
+++ api.js
@@ -0,0 +1,44 @@
+import Toys from 'toys'
+const ping = () => 'pong'
+
+const wrap = Toys.handler
+
+const GET = (path, handler) => ({method: 'GET', path, handler: wrap(handler)})
+const PUT = (path, handler) => ({method: 'PUT', path, handler: wrap(handler)})
+const POST = (path, handler) => ({method: 'POST', path, handler: wrap(handler)})
+
+const base = ({path, Model}) => [
+  {
+    ...GET(`/api/${path}`, async (req, reply) =>
+      reply(await Model.find().sort('sequence -date -delivery').populate('user').batchSize(10000)))
+  },
+  {
+    ...GET(`/api/${path}/code/{code}`, async (req, reply) =>
+      reply(await Model.findOne({code: req.params.code})))
+  },
+  {
+    ...GET(`/api/${path}/path`, async (req, reply) =>
+      reply(await Model.findOne({path: req.query.value})))
+  },
+  {
+    ...GET(`/api/${path}/{id}`, async (req, reply) =>
+      reply(await Model.findOne({_id: req.params.id})))
+  },
+  {
+    ...POST(`/api/${path}`, async (req, reply) => {
+      delete req.payload._id
+      const result = await new Model(req.payload).save()
+      reply(result)
+    })
+  },
+  {
+    ...PUT(`/api/${path}/{id}`, async (req, reply) => {
+      await Object.assign(
+        await Model.findOne({_id: req.params.id}), req.payload
+      ).save()
+      reply('Update complete')
+    })
+  }
+]
+
+export default {ping, base, GET, PUT, POST}
