fdcbd3dd1a99293de5a620ee4560bb72baf3dfd7
Jonas Windey <jonas.windey@gmail.com>
2017-10-25 17:25:03 +0200

    Add userhelper

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import api from './api'
 import db from './db'
 import server from './server'
+import userhelper from './userhelper'
 
-export default {api, db, server}
-export {api, db, server}
+export default {api, db, server, userhelper}
+export {api, db, server, userhelper}
---
+++ src/userhelper.js
@@ -0,0 +1,30 @@
+import crypto from 'crypto'
+
+const mongoose = require('mongoose')
+const User = mongoose.model('User')
+
+const hash = (salt, password) =>
+  crypto.pbkdf2Sync(password, new Buffer(salt, 'base64'), 10000, 64, 'sha1').toString('base64')
+
+const saltHash = password => {
+  const salt = crypto.randomBytes(16).toString('base64')
+  return {salt, hash: hash(salt, password)}
+}
+
+const register = async ({email, password, customer}) =>
+  new Promise(async (resolve, reject) => {
+    if (!email || !password || !customer) reject('Validation failed')
+    else if (await User.findOne({email})) reject('User already exists')
+    else resolve(await new User({email, customer, ...saltHash(password)}).save())
+  })
+
+const auth = async ({email, password}) =>
+  new Promise(async (resolve, reject) => {
+    if (!email || !password) return reject('Validation failed')
+    const user = await User.findOne({email})
+    if (!user) reject('No user found')
+    else if (hash(user.salt, password) === user.hash) resolve(user)
+    else reject('Incorrect username or password')
+  })
+
+export default {register, auth}
