b22165d388b515f2f90ecf4068503ac42dda50c1
Jonas Windey <jonas.windey@gmail.com>
2017-11-15 20:11:56 +0100

    Reject errors

--- src/userhelper.js
+++ src/userhelper.js
@@ -2,29 +2,29 @@ import crypto from 'crypto'
 import db from './db'
 
 const hash = (salt, password) =>
-  crypto.pbkdf2Sync(password, new Buffer(salt, 'base64'), 10000, 64, 'sha1').toString('base64')
+  crypto.pbkdf2Sync(password, Buffer.from(salt, 'base64'), 10000, 64, 'sha1').toString('base64')
 
 const saltHash = password => {
   const salt = crypto.randomBytes(16).toString('base64')
   return {salt, hash: hash(salt, password)}
 }
 
-const register = async ({email, password, customer}) =>
+const register = async ({email, password}) =>
   new Promise(async (resolve, reject) => {
     const User = db.model('User')
-    if (!email || !password || !customer) reject('Validation failed')
-    else if (await User.findOne({email})) reject('User already exists')
-    else resolve(await new User({email, customer, ...saltHash(password)}).save())
+    if (!email || !password) reject(new Error('Validation failed'))
+    else if (await User.findOne({email})) reject(new Error('User already exists'))
+    else resolve(await new User({email, ...saltHash(password)}).save())
   })
 
 const auth = async ({email, password}) =>
   new Promise(async (resolve, reject) => {
     const User = db.model('User')
-    if (!email || !password) return reject('Validation failed')
+    if (!email || !password) return reject(new Error('Validation failed'))
     const user = await User.findOne({email})
-    if (!user) reject('No user found')
+    if (!user) reject(new Error('No user found'))
     else if (hash(user.salt, password) === user.hash) resolve(user)
-    else reject('Incorrect username or password')
+    else reject(new Error('Incorrect username or password'))
   })
 
-export default {register, auth}
+export default {saltHash, register, auth}
