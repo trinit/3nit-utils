ee32fcc33ace04992904629df73cecdd4a45aa3f
Jonas Windey <jonas.windey@gmail.com>
2018-05-11 20:11:56 +0200

    Hapi 17

--- src/api.js
+++ src/api.js
@@ -1,51 +1,48 @@
-import Toys from 'toys'
-const wrap = Toys.handler
-
 const ping = () => `pong`
 
-const GET = (path, handler) => ({method: 'GET', path, handler: wrap(handler)})
-const PUT = (path, handler) => ({method: 'PUT', path, handler: wrap(handler)})
-const POST = (path, handler) => ({method: 'POST', path, handler: wrap(handler)})
-const DELETE = (path, handler) => ({method: 'DELETE', path, handler: wrap(handler)})
+const GET = (path, handler, auth) => ({method: 'GET', path, handler, config: {auth: auth}})
+const PUT = (path, handler, auth) => ({method: 'PUT', path, handler, config: {auth: auth}})
+const POST = (path, handler, auth) => ({method: 'POST', path, handler, config: {auth: auth}})
+const DELETE = (path, handler, auth) => ({method: 'DELETE', path, handler, config: {auth: auth}})
 
 const base = ({path, Model}) => [
   {
     ...GET(`/api/${path}`, async (req, reply) =>
-      reply(await Model.find().sort('sequence -date -created -delivery').populate('user').batchSize(10000)))
+      Model.find().sort('sequence -date -created -delivery').populate('user').batchSize(10000))
   },
   {
     ...GET(`/api/${path}/code/{code}`, async (req, reply) =>
-      reply(await Model.findOne({code: req.params.code})))
+      Model.findOne({code: req.params.code}))
   },
   {
     ...GET(`/api/${path}/path`, async (req, reply) =>
-      reply(await Model.findOne({path: req.query.value})))
+      Model.findOne({path: req.query.value}))
   },
   {
     ...GET(`/api/${path}/key`, async (req, reply) =>
-      reply(await Model.findOne({key: req.query.key})))
+      Model.findOne({key: req.query.key}))
   },
   {
     ...GET(`/api/${path}/{id}`, async (req, reply) =>
-      reply(await Model.findOne({_id: req.params.id})))
+      Model.findOne({_id: req.params.id}))
   },
   {
     ...POST(`/api/${path}`, async (req, reply) => {
       delete req.payload._id
       const result = await new Model(req.payload).save()
-      reply(result)
+      return result
     })
   },
   {
     ...DELETE(`/api/${path}/{id}`, async (req, reply) =>
-      reply(await Model.remove({_id: req.params.id})))
+      Model.remove({_id: req.params.id}))
   },
   {
     ...PUT(`/api/${path}/{id}`, async (req, reply) => {
       await Object.assign(
         await Model.findOne({_id: req.params.id}), req.payload
       ).save()
-      reply('Update complete')
+      return 'Update complete'
     })
   }
 ]
